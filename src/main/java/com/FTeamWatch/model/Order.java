63b05b5de29291717e9639131937afc748aecb03
TrongTai <nguyentrongtai.ait@gmail.com>
2022-05-21 00:39:23 +0700

    Finish creating entities and relationships

---
+++ src/main/java/com/FTeamWatch/model/Order.java
@@ -0,0 +1,50 @@
+package com.FTeamWatch.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "created_date")
+	private Date createdDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
+	@ManyToOne
+	@JoinColumn(name = "staff_id")
+	private Staff staff;
+	
+	@ManyToOne
+	@JoinColumn(name = "status_id")
+	private Status status;
+	
+	@OneToMany(mappedBy = "order")
+	private List<OrderDetail> orderDetails;
+	
+
+}
