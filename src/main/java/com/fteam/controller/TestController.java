5144fe17559e95979455e56467ac19900cb2ab55
TrongTai <nguyentrongtai.ait@gmail.com>
2022-05-23 21:14:44 +0700

    add client templates, config for thymeleaf

---
+++ src/main/java/com/fteam/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.fteam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("")
+	public String viewClientHomePage() {
+		return "index";
+	}
+
+}
