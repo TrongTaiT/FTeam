864860c2f243063f0559b60703062ac2b0020c46
TrongTai <nguyentrongtai.ait@gmail.com>
2022-05-23 19:29:00 +0700

    Add BrandRepository

---
+++ src/main/java/com/fteam/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.fteam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fteam.model.Brand;
+
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+
+}
