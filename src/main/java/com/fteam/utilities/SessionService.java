8dab32c042dde597945aab6cf53132e9157915b8
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-01 13:25:25 +0700

    upgrade SessionService.get()

--- src/main/java/com/fteam/utilities/SessionService.java
+++ src/main/java/com/fteam/utilities/SessionService.java
@@ -7,14 +7,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
-	
+
 	@Autowired
 	HttpSession session;
-	
+
 	/**
 	 * Đọc giá trị attribute trong session
+	 * 
 	 * @param name tên attribute
 	 * @return giá trị đọc được hoặc null nếu không tồn tại
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T get(String name) {
+		return (T) session.getAttribute(name);
+	}
+
+	/**
+	 * Đọc giá trị attribute trong session
+	 * 
+	 * @param name tên attribute
+	 * @param defaultValue giá trị mặc định nếu không tồn tại
+	 * @return giá trị đọc được
 	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(String name, Object defaultValue) {
@@ -24,23 +37,24 @@ public class SessionService {
 		}
 		return (T) value;
 	}
-	
+
 	/**
 	 * Thay đổi hoặc tạo mới attribute trong session
-	 * @param name tên attribute
+	 * 
+	 * @param name  tên attribute
 	 * @param value giá trị attribute
 	 */
 	public void set(String name, Object value) {
 		session.setAttribute(name, value);
 	}
-	
+
 	/**
 	 * Xoá attribute trong session
+	 * 
 	 * @param name tên attribute cần xoá
 	 */
 	public void remove(String name) {
 		session.removeAttribute(name);
 	}
-	
 
 }
